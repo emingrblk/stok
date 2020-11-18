e7a1fbebbad182964899c0120f3cb15adbacd57b
emin gürbulak <38581780+emingrblk@users.noreply.github.com>
2020-11-18 23:30:31 +0300

    create

---
+++ StokTakip/StokTakip/Controllers/UrunsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StokTakip.Models;
+
+namespace StokTakip.Controllers
+{
+    public class UrunsController : Controller
+    {
+        private StokTakipDbEntities db = new StokTakipDbEntities();
+
+        // GET: Uruns
+        public ActionResult Index()
+        {
+            return View(db.Urun.ToList());
+        }
+
+        // GET: Uruns/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urun urun = db.Urun.Find(id);
+            if (urun == null)
+            {
+                return HttpNotFound();
+            }
+            return View(urun);
+        }
+
+        // GET: Uruns/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Uruns/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,DermojetNo,UrunIsmi,Adet")] Urun urun)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Urun.Add(urun);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(urun);
+        }
+
+        // GET: Uruns/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urun urun = db.Urun.Find(id);
+            if (urun == null)
+            {
+                return HttpNotFound();
+            }
+            return View(urun);
+        }
+
+        // POST: Uruns/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,DermojetNo,UrunIsmi,Adet")] Urun urun)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(urun).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(urun);
+        }
+
+        // GET: Uruns/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urun urun = db.Urun.Find(id);
+            if (urun == null)
+            {
+                return HttpNotFound();
+            }
+            return View(urun);
+        }
+
+        // POST: Uruns/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Urun urun = db.Urun.Find(id);
+            db.Urun.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
